142ee9343c4fd5770fa1e5779ad0af76ebf29d7a
Filipe <Filipecasagrande1@gmail.com>
2025-04-15 23:21:38 -0300

    Add project files.

---
+++ Atividade 07 - Robo Tupiniquim/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Atividade_07___Robo_Tupiniquim
+{
+    internal class Program
+    {
+        // Comando EMEMEMEMEMM
+        // char [] instrucao = comando.tochararray();
+
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            char cardeais = 'N';
+            string comando = "EMEMEMEMM";
+            char[] orientacao = comando.ToCharArray();
+
+
+            for (int i = 0; i < orientacao.Length; i++)
+            {
+
+                if (orientacao[i] == 'E')
+                {
+                    if (cardeais == 'N')
+                    {
+                        cardeais = 'L';
+                    }
+                    else if (cardeais == 'L')
+                    {
+                        cardeais = 'S';
+                    }
+                    else if (cardeais == 'S')
+                    {
+                        cardeais = 'O';
+                    }
+                    else if (cardeais == 'O')
+                    {
+                        cardeais = 'N';
+                    }
+                }
+
+            }   
+            Console.WriteLine(cardeais);
+            Console.ReadLine();
+        }
+    }
+}
