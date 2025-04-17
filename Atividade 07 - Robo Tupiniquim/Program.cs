db631d6c38ace1c3dbf284fa9993233083225cc7
Filipe <Filipecasagrande1@gmail.com>
2025-04-17 20:02:18 -0300

    implementado versao estruturada

--- Atividade 07 - Robo Tupiniquim/Program.cs
+++ Atividade 07 - Robo Tupiniquim/Program.cs
@@ -2,9 +2,6 @@
 {
     internal class Program
     {
-        // Comando EMEMEMEMEMM
-        // char [] instrucao = comando.tochararray();
-
         static void Main(string[] args)
         {
             int x = 1, y = 2;
@@ -12,10 +9,16 @@
             string comando = "EMEMEMEMM";
             char[] orientacao = comando.ToCharArray();
 
+            Robo1(x, y, comando, orientacao, cardeais);
+
+            Robo2(x, y, comando, orientacao, cardeais);
+        }
+        static void Robo1(int x, int y, string comando, char[] orientacao, char cardeais)
+        {
 
             for (int i = 0; i < orientacao.Length; i++)
             {
-                if (orientacao[i] == 'E')
+                if (orientacao[i] == 'D')
                 {
                     if (cardeais == 'N')
                     {
@@ -34,7 +37,27 @@
                         cardeais = 'N';
                     }
                 }
-               
+
+                if (orientacao[i] == 'E')
+                {
+                    if (cardeais == 'N')
+                    {
+                        cardeais = 'O';
+                    }
+                    else if (cardeais == 'O')
+                    {
+                        cardeais = 'S';
+                    }
+                    else if (cardeais == 'S')
+                    {
+                        cardeais = 'L';
+                    }
+                    else if (cardeais == 'L')
+                    {
+                        cardeais = 'N';
+                    }
+                }
+
                 else if (orientacao[i] == 'M')
                 {
                     if (cardeais == 'N')
@@ -51,12 +74,90 @@
                     }
                     else if (cardeais == 'O')
                     {
-                        x -=1;
+                        x -= 1;
                     }
                 }
             }
+            Console.Write("As cordenadas do robô 1 são: ");
+            Console.WriteLine($"{x}, {y}, {cardeais}");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Precione enter para ver a localização do robô 2");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.ReadLine();
+            return;
+
+        }
+        static void Robo2(int x, int y, string comando, char[] orientacao, char cardeais)
+        {
+            x = 3; y = 3; comando = "MMDMMDMDDM"; cardeais = 'L';
+            orientacao = comando.ToCharArray();
+
+            for (int i = 0; i < orientacao.Length; i++)
+            {
+                if (orientacao[i] == 'D')
+                {
+                    if (cardeais == 'N')
+                    {
+                        cardeais = 'L';
+                    }
+                    else if (cardeais == 'L')
+                    {
+                        cardeais = 'S';
+                    }
+                    else if (cardeais == 'S')
+                    {
+                        cardeais = 'O';
+                    }
+                    else if (cardeais == 'O')
+                    {
+                        cardeais = 'N';
+                    }
+                }
+
+                if (orientacao[i] == 'E')
+                {
+                    if (cardeais == 'N')
+                    {
+                        cardeais = 'O';
+                    }
+                    else if (cardeais == 'O')
+                    {
+                        cardeais = 'S';
+                    }
+                    else if (cardeais == 'S')
+                    {
+                        cardeais = 'L';
+                    }
+                    else if (cardeais == 'L')
+                    {
+                        cardeais = 'N';
+                    }
+                }
+
+                else if (orientacao[i] == 'M')
+                {
+                    if (cardeais == 'N')
+                    {
+                        y += 1;
+                    }
+                    else if (cardeais == 'L')
+                    {
+                        x += 1;
+                    }
+                    else if (cardeais == 'S')
+                    {
+                        y -= 1;
+                    }
+                    else if (cardeais == 'O')
+                    {
+                        x -= 1;
+                    }
+                }
+            }
+            Console.Write("As cordenadas do robô 2 são: ");
             Console.WriteLine($"{x}, {y}, {cardeais}");
             Console.ReadLine();
+            return;
         }
     }
 }
---
+++ Atividade 07 - Robo Tupiniquim/Robô2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_07___Robo_Tupiniquim
+{
+    class Robo2
+    {
+
+
+
+    }
+}
